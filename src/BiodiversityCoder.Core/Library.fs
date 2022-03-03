f511b2b3c3ac7cb1c46d09cebef66351cf22de48
Andrew Martin <AndrewIOM@users.noreply.github.com>
2022-03-03 14:41:20 +0000

    Use Bolero F# component

---
+++ src/BiodiversityCoder.Core/Library.fs
@@ -0,0 +1,33 @@
+﻿namespace BiodiversityCoder.Core
+
+open Elmish
+open Bolero
+open Bolero.Html
+
+module Test =
+
+    type Model =
+        {
+            x: string
+        }
+
+    let initModel =
+        {
+            x = ""
+        }
+
+    type Message =
+        | Ping
+
+    let update message model =
+        match message with
+        | Ping -> model
+
+    let view model dispatch =
+        text "Hello, world!"
+
+type MainApp() =
+    inherit ProgramComponent<Test.Model, Test.Message>()
+
+    override this.Program =
+        Program.mkSimple (fun _ -> Test.initModel) Test.update Test.view
