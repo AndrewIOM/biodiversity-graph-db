6b22fdd683aaac5efba81af13a856ed8fda1ee75
Andrew Martin <AndrewIOM@users.noreply.github.com>
2024-09-04 14:40:13 +0100

    Net8.0 (#13)
    * Update from net6 to net8
    
    * Bump v1.9.1
    
    * use sonoma mac runner

--- src/BiodiversityCoder/MauiProgram.cs
+++ src/BiodiversityCoder/MauiProgram.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using BiodiversityCoder.Core;
+﻿using BiodiversityCoder.Core;
+using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Storage;
 
 namespace BiodiversityCoder;
 
@@ -10,19 +11,29 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
-
-		#if WINDOWS
-				builder.Services.AddTransient<IFolderPicker, Platforms.Windows.FolderPicker>();
-		#elif MACCATALYST
-				builder.Services.AddTransient<IFolderPicker, Platforms.MacCatalyst.FolderPicker>();
-		#endif
 		
+		builder.Services.AddTransient<BiodiversityCoder.Core.IFolderPicker, CrossPlatformFolderPicker>();
 		builder.Services.AddMauiBlazorWebView();
 		
+		#if DEBUG
+			builder.Services.AddBlazorWebViewDeveloperTools();
+		#endif
+
 		return builder.Build();
 	}
+}
+
+public class CrossPlatformFolderPicker() : BiodiversityCoder.Core.IFolderPicker
+{
+    public async Task<string> PickFolder(CancellationToken cancellationToken)
+    {
+        var result = await FolderPicker.Default.PickAsync(cancellationToken);
+        result.EnsureSuccess();
+		return result.Folder.Path;
+    }
 }
